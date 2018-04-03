376382457f7a6adf62eca06e901ff404baeb6434
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-03 00:31:36 -0700

    RESTful routes for all three controllers added. (Actual controllers still need to be added, however.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/drivers', to: 'drivers#index', as: 'drivers'
+  get '/drivers/new', to: 'drivers#new', as: 'new-driver'
+  post '/drivers', to: 'drivers#create'
+  get '/drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
+  get '/drivers/:id', to: 'drivers#show', as: 'driver'
+  patch '/drivers/:id', to: 'drivers#update'
+  delete '/drivers/:id', to: 'drivers#destroy'
+
+  get '/passengers', to: 'passengers#index', as: 'passengers'
+  get '/passengers/new', to: 'passengers#new', as: 'new-passenger'
+  post '/passengers', to: 'passengers#create'
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+  patch '/passengers/:id', to: 'passengers#update'
+  delete '/passengers/:id', to: 'passengers#destroy'
+
+  get '/trips', to: 'trips#index', as: 'trips'
+  get '/trips/new', to: 'trips#new', as: 'new-trip'
+  post '/trips', to: 'trips#create'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+  get '/trips/:id', to: 'trips#show', as: 'trip'
+  patch '/trips/:id', to: 'trips#update'
+  delete '/trips/:id', to: 'trips#destroy'
+
+
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
