e93a21d71fdb2c96205da6355e6a79f741922855
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-04 08:15:59 -0700

    average rating and total earnings methods roughed out for the Driver model. No testing done, though: Not at all sure if any of this works.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,20 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true
+
+  def overall_earnings
+    accum = 0
+    @driver.trips.each do |trip|
+      accum += trip.cost
+    end
+    earnings = (accum - 1.65) * 0.8
+  end
+
+  def average_rating
+    ratings_total = 0
+    @driver.trips.each do |trip|
+      ratings_total += trip.rating
+    end
+    ratings_average = ratings_total / @driver.trips.count
+  end
 end
