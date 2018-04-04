1c60392f5cf6e63047be10426d930415c081f267
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-04 14:30:20 -0700

    Got business logic for the drivers working, and adjusted the show page to make it visible. No styling of it, yet, though.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,19 +3,20 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence: true
 
-  def overall_earnings
-    accum = 0
-    @driver.trips.each do |trip|
-      accum += trip.cost
+  def overall_earnings(id)
+    revenue_total = 0
+    Driver.find_by(id:id).trips.each do |trip|
+      revenue_total += trip.cost
     end
-    earnings = (accum - 1.65) * 0.8
+    earnings = (((revenue_total - 1.65) * 0.8) / 100 ).round(2)
   end
 
-  def average_rating
+  def average_rating(id)
     ratings_total = 0
+    @driver = Driver.find_by(id:id)
     @driver.trips.each do |trip|
-      ratings_total += trip.rating
+      ratings_total += trip.rating.to_f
     end
-    ratings_average = ratings_total / @driver.trips.count
+    ratings_average = (ratings_total / @driver.trips.count).round(1)
   end
 end
